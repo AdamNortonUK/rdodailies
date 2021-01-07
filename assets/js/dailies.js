8a71411c7fd1a22774b0b157f344d760266f3a63
Adam Norton <40515527+AdamNortonUK@users.noreply.github.com>
2021-01-07 12:46:19 +0000

    Update dailies.js

--- assets/js/dailies.js
+++ assets/js/dailies.js
@@ -22,15 +22,30 @@ var checkboxValue = JSON.parse(localStorage.getItem('checkboxValue')) || {}
     $.each(checkboxValue, function(key, value) {
       $("#" + key).prop('checked', value);
     });
+	
+	var checkboxValueGeneral = JSON.parse(localStorage.getItem('checkboxValueGeneral')) || {}
+    var $checkboxGeneral = $(".general-challenge-input-container :checkbox");
+
+    $checkboxGeneral.on("change", function() {
+      $checkboxGeneral.each(function() {
+        checkboxValueGeneral[this.id] = this.checked;
+      });
+      localStorage.setItem("checkboxValueGeneral", JSON.stringify(checkboxValueGeneral));
+    });
+
+    //on page load
+    $.each(checkboxValueGeneral, function(key, value) {
+      $("#" + key).prop('checked', value);
+    });
   
-$('.challenge-input-checkbox').on('change', function(){
+$('.general-challenge-input-checkbox').on('click', function(){
     var GeneralChecked = 0;
-    $.each($('.challenge-input-checkbox'), function(){
+    $.each($('.general-challenge-input-checkbox'), function(){
         if($(this).is(':checked')){
             GeneralChecked++;    
         }
     });
-    if(GeneralChecked >= 22){
+    if(GeneralChecked >= 7){
         $.each($('.challenge-input-checkbox-general'), function(){
             if($(this).not(':checked').length == 1){
                 $(this).prop('checked','checked');    
@@ -41,14 +56,14 @@ $('.challenge-input-checkbox').on('change', function(){
     }
 });
   
-$('.challenge-input-checkbox').on('change', function(){
+$('.challenge-input-checkbox').on('click', function(){
     var RolesChecked = 0;
     $.each($('.challenge-input-checkbox'), function(){
         if($(this).is(':checked')){
             RolesChecked++;    
         }
     });
-    if(RolesChecked >= 22){
+    if(RolesChecked >= 9){
         $.each($('.challenge-input-checkbox-roles'), function(){
             if($(this).not(':checked').length == 1){
                 $(this).prop('checked','checked');    
@@ -63,6 +78,7 @@ $('.challenge-input-checkbox').on('change', function(){
     
     var DailyChallengesIcons = '/assets/images/daily-challenge-icons/';
     var DailyChallengesCompleted = $("input[class='challenge-input-checkbox']:not(':checked')").length;
+    var DailyChallengesCompleted2 = $("input[class='general-challenge-input-checkbox']:not(':checked')").length;
     var GeneralChallengesCompleted = $("input[name='GeneralChallenge']:not(':checked')").length;
     var BountyChallengesCompleted = $("input[name='BountyChallenge']:not(':checked')").length;
     var TraderChallengesCompleted = $("input[name='TraderChallenge']:not(':checked')").length;  
@@ -81,9 +97,11 @@ $('.challenge-input-checkbox').on('change', function(){
     var RoleDailyChallengesCompleted = BountyRoleChallengesCompleted + TraderRoleChallengesCompleted + CollectorRoleChallengesCompleted + MoonshinerRoleChallengesCompleted + NaturalistRoleChallengesCompleted;
     
     $("#general-challenges-total").text(GeneralDailyChallengesCompleted);    
-    $("#roles-challenges-total").text(RoleDailyChallengesCompleted);
-      
-    $("#daily-challenges-counter-icon").prop('src', DailyChallengesIcons + 'daily_challenge_toast_' + DailyChallengesCompleted + '.png');
+    $("#roles-challenges-total").text(RoleDailyChallengesCompleted);	  
+
+	var TotalChallengesCompleted = DailyChallengesCompleted + DailyChallengesCompleted2;      
+
+	$("#daily-challenges-counter-icon").prop('src', DailyChallengesIcons + 'daily_challenge_toast_' + TotalChallengesCompleted + '.png');
     $("#daily-challenges-counter-widget-icon").prop('src', DailyChallengesIcons + 'daily_challenge_toast_' + DailyChallengesCompleted + '.png');
     $("#daily-challenges-counter-title-icon").prop('src', DailyChallengesIcons + 'daily_challenge_toast_' + DailyChallengesCompleted + '.png');    
     $("#general-challenges-counter-icon").prop('src', DailyChallengesIcons + 'daily_challenge_toast_' + GeneralChallengesCompleted + '.png');
@@ -104,32 +122,44 @@ $('.challenge-input-checkbox').on('change', function(){
   }
   
 DailyChallengesCounter();
-  $(":checkbox").click(DailyChallengesCounter);
+  $(":checkbox").change(DailyChallengesCounter);
   
   
   function DailyChallengesGoldCounter() {
     
     
-    var DailyChallengesGoldCount = $("input[class='challenge-input-checkbox']:checked").length;
+    var GeneralChallengesGoldCount = $("input[class='general-challenge-input-checkbox']:checked").length;    
+    var RolesChallengesGoldCount = $("input[class='challenge-input-checkbox']:checked").length;    
+    var DailyChallengesGoldCount = (GeneralChallengesGoldCount + RolesChallengesGoldCount);      
     var RoleChallengesCompletedGoldCount = $("input[class='challenge-input-checkbox-roles']:checked").length;
     var GeneralChallengesCompletedGoldCount = $("input[class='challenge-input-checkbox-general']:checked").length;
     var DailyChallengesGoldMultiplerValue = document.getElementById("DailyChallengesGoldMultipler");
     var DailyChallengesGoldValue = DailyChallengesGoldMultiplerValue.options[DailyChallengesGoldMultiplerValue.selectedIndex].value;
-    var DailyRoleChallengesGold = DailyChallengesGoldValue * RoleChallengesCompletedGoldCount *3;
-    var DailyGeneralChallengesGold = DailyChallengesGoldValue * GeneralChallengesCompletedGoldCount *3;
+    var DailyRoleChallengesGold = (DailyChallengesGoldValue * RoleChallengesCompletedGoldCount * 3);
+    var DailyGeneralChallengesGold = (DailyChallengesGoldValue * GeneralChallengesCompletedGoldCount * 3);
     
     
     //var DailyChallengesGold = DailyChallengesGoldValue * 10 * DailyChallengesGoldCount / 10;
+   
+    var DailyChallengesGoldTotal = (DailyGeneralChallengesGold + DailyRoleChallengesGold);
     
-    var DailyChallengesGoldTotal = DailyGeneralChallengesGold + DailyRoleChallengesGold;
-    
-    var DailyChallengesGold = DailyChallengesGoldTotal + DailyChallengesGoldValue * 10 * DailyChallengesGoldCount / 10;
-    
-    $("#daily-challenges-gold").text('' + DailyChallengesGold + ' ');
+    var DailyChallengesGold = ((DailyChallengesGoldTotal + DailyChallengesGoldValue * 10 * DailyChallengesGoldCount / 10).toFixed(2));	  
+
+	$("#daily-challenges-gold").text('' + DailyChallengesGold + ' ');
   
   }
 
 DailyChallengesGoldCounter();
-  $(":checkbox").click(DailyChallengesGoldCounter);	
-	
+  $(":checkbox").change(DailyChallengesGoldCounter);	
+
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+   var input = document.getElementById('DailyChallengesGoldMultipler');
+   if (localStorage['DailyChallengesGoldMultipler']) { // if job is set
+       input.value = localStorage['DailyChallengesGoldMultipler']; // set the value
+   }
+   input.onchange = function () {
+        localStorage['DailyChallengesGoldMultipler'] = this.value; // change localStorage on change
+    }
 });
