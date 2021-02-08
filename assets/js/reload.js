7aadea2cc7c0f752cef2e0c2f0148490f6903fa5
Michal__d <53737762+MichalD96@users.noreply.github.com>
2021-02-08 04:46:26 +0100

    Reload site automatically on dailies change

--- assets/js/reload.js
+++ assets/js/reload.js
@@ -1,14 +1,34 @@
-function refreshData() {
-	var previous = null;
-	var current = null;
-
-	$.getJSON("https://rdocdn.xyz/api/dailies.php", function (json) {
-		current = JSON.stringify(json);
-		if (previous && current && previous !== current) {
-			console.log('refresh');
-			location.reload();
-		}
-		previous = current;
-	});
+async function fetchData(url) {
+	const queryString = { method: "GET", cache: "no-cache" };
+	queryString.nocache = Date.now();
+	try {
+		const response = await fetch(url, queryString);
+		return await response.json();
+	} catch (err) {
+		throw new Error(`Failed to load: ${url}\n${err}`);
+	}
 }
-setInterval(refreshData, 2000);
+
+((checkTime) => {
+	const isValid = date => date.indexOf(new Date(Date.now() - 216e5).toISOString().split('T')[0]) !== -1;
+	let apiDate = null;
+	fetchData('https://rdocdn.xyz/api/dailies.php')
+		.then((data) => {
+			if (!data.date) reject('data.date does not exists');
+			apiDate = data.date;
+		})
+		.then(() => {
+			setInterval(async () => {
+				if (!isValid(apiDate)) {
+					await wait(checkTime);
+					location.reload(true);
+				}
+			}, checkTime);
+		})
+		.catch((e) => new Error(e));
+})(10000);
+
+
+function wait(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
